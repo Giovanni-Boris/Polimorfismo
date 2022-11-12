4d853f0e3fa6d36765673f09cf4a1d6269bdf0cf
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-12 11:37:05 -0500

    Clase gato

---
+++ herencia/Gato.cc
@@ -0,0 +1,9 @@
+#include "Gato.h"
+#include <iostream>
+void Gato::hablar(){
+  std::cout<<"SOy un gato\n";
+}
+
+void Gato::foo(){
+  std::cout<<"Foo con gato\n";
+}
---
+++ herencia/Gato.h
@@ -0,0 +1,10 @@
+#ifndef GATO_H
+#define GATO_H
+#include "Animal.h"
+class Gato: public Animal{
+  public:
+    void hablar();
+    void foo();
+};
+
+#endif
