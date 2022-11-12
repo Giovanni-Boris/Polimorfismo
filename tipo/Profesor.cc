e9300dd2c3be9c1848d1d5d6969f746b1a59e687
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-12 11:46:33 -0500

    Clase profesor

---
+++ tipo/Profesor.cc
@@ -0,0 +1,11 @@
+#include "Profesor.h"
+#include <string>
+#include <iostream>
+using namespace std;
+void Profesor::planilla(){
+  string a = "Profesor Desde 2002 \n SUeldo 2200 \n Sede: COlegio UPC\n";
+  cout<<a;
+}
+void Profesor::foo(){
+  cout<< "Foo con profesor\n";
+}
---
+++ tipo/Profesor.h
@@ -0,0 +1,11 @@
+#ifndef PROFESOR_H
+#define PROFESOR_H
+#include "Comisionado.h"
+class Profesor: public Comisionado{
+  public:
+    void planilla();
+    void foo();
+};
+
+
+#endif
