37bea0deae6a9623d5e203fd394e25d2cf34b930
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-12 11:44:45 -0500

    Claso empelado parcial

---
+++ tipo/EmpleadoParcial.cc
@@ -0,0 +1,8 @@
+#include "EmpleadoParcial.h"
+#include <iostream>
+void Empleado_Parcial::hablar(){
+  std::cout<<"Soy un empleado parcial\n";
+}
+void Empleado_Parcial::goo(){
+  std::cout<<"Goo en empleado Parcial\n";
+}
---
+++ tipo/EmpleadoParcial.h
@@ -0,0 +1,11 @@
+#ifndef EMPLEADO_PARCIAL_H
+#define EMPLEADO_PARCIAL_H
+#include "Empleado.h"
+class Empleado_Parcial: public Empleado{
+  public:
+    void hablar();
+    void goo();
+};
+
+
+#endif
