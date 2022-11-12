7481528489b345a84b5924b1dbe08dd709ebc61e
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-12 11:38:41 -0500

    CLase persona

---
+++ herencia/Persona.cc
@@ -0,0 +1,9 @@
+#include "Persona.h"
+#include <iostream>
+void Persona::saludar(int age){
+  std::cout << "Saludo con la mano y edad es "<<age<<"\n";
+}
+
+void Persona::foo(){
+  std::cout << "foo con persona\n";
+}
---
+++ herencia/Persona.h
@@ -0,0 +1,11 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+#include "Saludo.h"
+class Persona: public Saludo{
+  public:
+    void saludar(int);
+    void foo();
+};
+
+
+#endif
