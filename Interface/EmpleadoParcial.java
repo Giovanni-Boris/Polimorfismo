2a31550f2b5d2019fc7983c4f56dc469d93cf175
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-13 11:53:42 -0500

    Clase Empleado Parcial2

---
+++ Interface/EmpleadoParcial.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sin.plimorfismo;
+
+/**
+ *
+ * @author H A L I O N
+ */
+public class EmpleadoParcial extends Empleado {
+    public void hablar(){
+        System.out.println("Soy un empleado parcial");
+    }
+    public void goo(){
+        System.out.println(" Goo en empelado Parcial");
+    }
+    
+    
+}
