538a7cfb494eb9941884c5d198420838771d274f
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-13 11:53:59 -0500

    Clase Profesor

---
+++ Interface/Profesor.java
@@ -0,0 +1,11 @@
+
+package sin.plimorfismo;
+
+public class Profesor implements Comisionado {
+    public void planilla(){
+        System.out.println("Profesor Desde 2002 \n SUeldo 2200 \n Sede: COlegio UPC");
+    }
+    public void foo(){
+        System.out.println(" Foo con profesor");
+    }
+}
