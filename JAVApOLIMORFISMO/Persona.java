0974572bc1d9ac7dd9cb4c7bbbd343d6f1d4621f
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-13 11:50:30 -0500

    Clase person2

---
+++ JAVApOLIMORFISMO/Persona.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.polimorfismo;
+
+public class Persona implements Saludo {
+    public void saludar(String part){
+        System.out.println("Saludar con la "+part+" y tengo 6 años");
+    }
+}
