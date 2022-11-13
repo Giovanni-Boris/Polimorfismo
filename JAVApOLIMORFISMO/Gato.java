eb44342df5e396eef1ec302d843322e31312b675
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-13 11:49:09 -0500

    Clase Gato

---
+++ JAVApOLIMORFISMO/Gato.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.polimorfismo;
+
+public class Gato extends Animal{
+    public Gato(String name){
+        super(name);
+    }
+    public String toString(){
+        return "Soy un gato";
+    }
+    
+}
