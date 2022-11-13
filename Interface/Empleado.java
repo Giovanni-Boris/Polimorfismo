dc69932c74ea90013642980bc361532d2b04c654
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-11-13 11:53:07 -0500

    Clase empleado

---
+++ Interface/Empleado.java
@@ -0,0 +1,17 @@
+
+package sin.plimorfismo;
+
+
+public class Empleado {
+    private int codigo;
+    public void hablar(){
+       System.out.println("Soy un empleado");
+    };
+    public int getCodigo(){
+        return codigo;
+    };
+    public void goo(){
+        System.out.println("Goo con empleado");
+    }
+    
+}
